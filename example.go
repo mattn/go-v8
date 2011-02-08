1fc7c6b1b7539481e50accc8c300fb868391206a
mattn <mattn.jp@gmail.com>
2011-02-08 20:47:25 +0900

    example

---
+++ example.go
@@ -0,0 +1,28 @@
+package main
+
+import "v8"
+import "reflect"
+
+func main() {
+	v8ctx := v8.NewV8Context()
+	ret, err := v8ctx.Eval(`
+var a = 1;
+a += 2;
+a;
+`)
+	if err != nil {
+		println(err.String())
+	} else {
+		println(ret.(float64))
+	}
+
+	ret, err = v8ctx.Eval(`
+a+'b'
+`)
+	println(reflect.NewValue(ret).Type().Name())
+	if err != nil {
+		println(err.String())
+	} else {
+		println(ret.(string))
+	}
+}
