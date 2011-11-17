515a9b1b9c93aeb02267f05aaac8c63a97d2eff8
mattn <mattn.jp@gmail.com>
2011-11-17 15:22:38 +0900

    ooops.

---
+++ v8wrap.h
@@ -0,0 +1,15 @@
+#ifndef _V8WRAP_H_
+#ifdef __cplusplus
+extern "C" {
+#endif
+extern void v8_init(void* callback);
+extern void* v8_create();
+extern void v8_release(void* ctx);
+extern char* v8_execute(void* ctx, char* str);
+extern char* v8_error(void* ctx);
+
+typedef char* (*v8wrap_callback)(unsigned int, char*, char*);
+#ifdef __cplusplus
+}
+#endif
+#endif /* _V8WRAP_H_ */
