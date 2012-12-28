37f49fee790a42e579599164c8c8bfe4a56aa912
Jason Del Ponte <info@jasondelponte.com>
2012-12-28 13:46:02 -0800

    Go fmt'ed source, and added javascript's variadic argument call of console.log to example.

--- example/example.go
+++ example/example.go
@@ -10,10 +10,11 @@ func main() {
 
 	// setup console.log()
 	v8ctx.Eval(`
-	this.console = { "log": function(args) { _console_log(args) }}`)
-	v8ctx.AddFunc("_console_log", func(args... interface{}) interface{} {
-		for _, arg := range args {
-			fmt.Printf("%v ", arg)
+	this.console = { "log": function(args) { _console_log.apply(null, arguments) }}`)
+	v8ctx.AddFunc("_console_log", func(args ...interface{}) interface{} {
+		fmt.Printf("Go console log: ")
+		for i := 0; i < len(args); i++ {
+			fmt.Printf("%v ", args[i])
 		}
 		fmt.Println()
 		return ""
@@ -25,8 +26,8 @@ func main() {
 	a += 2;
 	a;
 	`)
-	println(int(ret.(float64))) // 3
+	fmt.Println("Eval result:", int(ret.(float64))) // 3
 
-	v8ctx.Eval(`console.log(a + '年' + b + '組 金八先生！')`)  // 3b
-	v8ctx.Eval(`console.log("Hello World, こんにちわ世界")`) // john
+	v8ctx.Eval(`console.log(a + '年' + b + '組 金八先生！', 'something else')`) // 3b
+	v8ctx.Eval(`console.log("Hello World, こんにちわ世界")`)                    // john
 }
